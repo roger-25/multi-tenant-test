5a2a7becdf5d9ffd32206bc0b2b9eb90fb099b6c
roger-25 <devopsroger@gmail.com>
2025-04-11 11:28:30 +0530

    updated ngnix_deploy file

--- eks/nginx_deploy.tf
+++ eks/nginx_deploy.tf
@@ -1,125 +1,43 @@
-
 resource "kubernetes_deployment" "nginx" {
-  count = 2
-
   metadata {
-    name      = "nginx-${count.index}"
+    name      = "nginx"
     namespace = "default"
     labels = {
-      app = "nginx-${count.index}"
+      app = "nginx"
     }
   }
 
   spec {
-    replicas = 1
+    replicas = 2
+
     selector {
       match_labels = {
-        app = "nginx-${count.index}"
+        app = "nginx"
       }
     }
+
     template {
       metadata {
         labels = {
-          app = "nginx-${count.index}"
+          app = "nginx"
         }
       }
+
       spec {
         container {
-      resources {
-        limits = {
-          cpu = "500m"
-          memory = "512Mi"
-        }
-        requests = {
-          cpu = "200m"
-          memory = "256Mi"
-        }
-      }
           name  = "nginx"
           image = "nginx:latest"
-        }
-      }
-    }
-  }
-}
 
-
-resource "kubernetes_service" "nginx_service" {
-  metadata {
-    name      = "nginx-service"
-    namespace = "default"
-    labels = {
-      app = "nginx"
-    }
-  }
-
-  spec {
-    selector = {
-      app = "nginx"
-    }
-
-    port {
-      port        = 80
-      target_port = 80
-    }
-
-    type = "NodePort"
-  }
-}
-
-resource "kubernetes_ingress_v1" "nginx_ingress" {
-  metadata {
-    name      = "nginx-ingress"
-    namespace = "default"
-    annotations = {
-      "kubernetes.io/ingress.class" = "alb"
-      "alb.ingress.kubernetes.io/scheme" = "internet-facing"
-    }
-  }
-
-  spec {
-    rule {
-      http {
-        path {
-          path     = "/*"
-          path_type = "ImplementationSpecific"
-
-          backend {
-            service {
-              name = kubernetes_service.nginx_service.metadata[0].name
-              port {
-                number = 80
-              }
+          resources {
+            limits = {
+              cpu    = "500m"
+              memory = "512Mi"
+            }
+            requests = {
+              cpu    = "200m"
+              memory = "256Mi"
             }
           }
-        }
-      }
-    }
-  }
-}
-resource "kubernetes_horizontal_pod_autoscaler_v2" "nginx_hpa" {
-  metadata {
-    name = "nginx-hpa"
-    namespace = "default"
-  }
-
-  spec {
-    scale_target_ref {
-      api_version = "apps/v1"
-      kind        = "Deployment"
-      name        = kubernetes_deployment.nginx.metadata[0].name
-    }
-
-    min_replicas = 2
-    max_replicas = 5
-
-    metric {
-      type = "Resource"
-      resource {
-        name = "cpu"
-        target {
-          type               = "Utilization"
-          average_utilization = 60
         }
       }
     }
