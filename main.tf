51c7b53b9e666adf6d54839b85e38d67be91e088
roger-25 <devopsroger@gmail.com>
2025-04-11 11:41:03 +0530

    updated main

--- main.tf
+++ main.tf
@@ -4,6 +4,7 @@ terraform {
     key            = "terraform-state/${terraform.workspace}/eks.tfstate"
     region         = "us-east-1"
     dynamodb_table = "multi-tenant-db"
+    encrypt = true
   }
 }
 
