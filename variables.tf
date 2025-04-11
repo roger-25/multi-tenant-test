ab1f2e6ec10e3fade93224767b1d4a728b70eca4
roger-25 <devopsroger@gmail.com>
2025-04-11 15:25:38 +0530

    updating yaml file for tfvars files6

--- variables.tf
+++ variables.tf
@@ -1,36 +1,15 @@
-variable "cluster_name" {
-  description = "Name of the EKS cluster"
-  type        = string
-}
+variable "cluster_name" {}
 
-variable "region" {
-  description = "AWS region"
-  type        = string
-  default     = "us-east-1"
-}
+variable "region" {}
 
 # For network overrides (optional)
-variable "vpc_id" {
-  description = "Optional custom VPC ID (overrides remote state)"
-  type        = string
-  default     = null
-}
+variable "vpc_id" {}
 
-variable "subnet_ids" {
-  description = "Optional custom subnet IDs (overrides remote state)"
-  type        = list(string)
-  default     = null
-}
+variable "subnet_ids" {}
 
 variable "ecr_repository_name" {}
 variable "image_tag" {}
 
-variable "tenant_name" {
-  description = "Name of the tenant"
-  type        = string
-}
+variable "tenant_name" {}
 
-variable "tenant_env" {
-  description = "Tenant environment (dev/uat/prod)"
-  type        = string
-}
+variable "tenant_env" {}
