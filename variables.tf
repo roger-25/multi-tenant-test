3dda79490d527477d7c48a1c5d00afd10f6b8242
roger-25 <devopsroger@gmail.com>
2025-04-11 14:30:18 +0530

    updating yaml file for tfvars files2

--- variables.tf
+++ variables.tf
@@ -8,3 +8,16 @@ variable "region" {
   type        = string
   default     = "us-east-1"
 }
+
+# For network overrides (optional)
+variable "vpc_id" {
+  description = "Optional custom VPC ID (overrides remote state)"
+  type        = string
+  default     = null
+}
+
+variable "subnet_ids" {
+  description = "Optional custom subnet IDs (overrides remote state)"
+  type        = list(string)
+  default     = null
+}
